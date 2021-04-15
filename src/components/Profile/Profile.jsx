39fd47ab33b4bb30d3ebbc910d95b7f2831f43b8
MaxKliuba <klyubamaks@gmail.com>
2021-04-15 21:16:36 +0300

    adaptive design added

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,26 +3,20 @@ import styles from './Profile.module.css';
 function Profile() {
     return (
         <div className={styles.content}>
+            <h3 className={styles.status}>Online</h3>
             <div className={styles.content_box}>
-                Main content
-            </div>
-            <div className={styles.content_box}>
-                <h1>lol</h1>
-                <h1>lol</h1>
-                <h1>lol</h1>
-                <h1>lol</h1>
-            </div>
-            <div className={styles.content_box}>
-                <h1>lol</h1>
-                <h1>lol</h1>
-                <h1>lol</h1>
-                <h1>lol</h1>
-            </div>
-            <div className={styles.content_box}>
-                <h1>lol</h1>
-                <h1>lol</h1>
-                <h1>lol</h1>
-                <h1>lol</h1>
+                <img className={styles.avatar} src="https://avatarfiles.alphacoders.com/150/thumb-150316.jpg" alt="avatar"></img>
+                <div className={styles.profile_info}>
+                    <h2 className={styles.user_name}>Test User Name</h2>
+                    <div className={styles.group}>
+                        <label className={styles.group_label}>Birthday:</label>
+                        <input className={styles.group_text} type="text" readOnly value="31.02.2000"/>
+                    </div>
+                    <div className={styles.group}>
+                        <label className={styles.group_label}>City:</label>
+                        <input className={styles.group_text} type="text" readOnly value="Test-City"/>
+                    </div>                   
+                </div>
             </div>
             <div className={styles.content_box}>
                 <h1>lol</h1>
