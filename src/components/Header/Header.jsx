e13660f11f252a497b337fd5580520a862d2b76d
MaxKliuba <klyubamaks@gmail.com>
2021-05-04 12:36:48 +0300

    css updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,16 +8,23 @@ function Header(props) {
     <header className={styles.header}>
       <img className={styles.logo} src={logo} alt="logo" />
       <h1 className={styles.title}>Neuron</h1>
-      {props.isAuth ? (
-        <div>
-          <span>{props.login}</span>
+
+      <div className={styles.header_button}>
+        {props.isAuth ? (
+          <div>
+            <span>{props.login}</span>
+          </div>
+        ) : (
+          <div className={styles.login_button}>
+            <NavLink to={"/login"}>Login</NavLink>
+          </div>
+        )}
+      </div>
+
+      {/* <span>{props.login}</span>
           <div className={styles.menu_button}>
             <div className={styles.menu_button_content}></div>
-          </div>
-        </div>
-      ) : (
-        <NavLink to={"/login"}>Login</NavLink>
-      )}
+          </div> */}
     </header>
   );
 }
